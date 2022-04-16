4734872cc45eab0e6da7c5fba57342d430ef7379
ericly <ericlyeric@gmail.com>
2022-04-16 16:47:08 -0700

    add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ const App = ({ Component, pageProps }: AppProps) => {
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 };
