5817ab6400c84f8d171eba5156e08bd7f554a943
ericly <ericlyeric@gmail.com>
2022-04-17 00:02:00 -0700

    move files

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,12 +1,13 @@
-import { default as Link } from "../common/Link";
+import { default as Link } from "./Link";
 import siteMetadata from "../helper/siteMetadata";
 import headerNavLinks from "../helper/headerNavLinks";
-import ThemeSwitch from "../common/ThemeSwitch";
-import MobileNav from "../common/MobileNav";
+import ThemeSwitch from "./ThemeSwitch";
+import MobileNav from "./MobileNav";
+import { BookOpenHeroIcon } from "../helper/Icons";
 
 const Header = () => {
   return (
-    <div className="flex h-screen flex-col justify-between">
+    <div className="flex flex-col justify-between">
       <header className="flex items-center justify-between py-10">
         <div className="flex items-center">
           <Link href="/">
@@ -41,22 +42,3 @@ const Header = () => {
 };
 
 export default Header;
-
-const BookOpenHeroIcon = () => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-6 w-6"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      strokeWidth="2"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
-      />
-    </svg>
-  );
-};
